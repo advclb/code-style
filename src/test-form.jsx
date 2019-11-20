b67d36d685b656ac01baa0a92202779d292efde7
Guo Yunhe <i@guoyunhe.me>
2019-11-21 01:06:01 +0200

    Test form

---
+++ src/test-form.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function TestForm() {
+  return (
+    <div>
+      <label htmlFor="name-input">Name</label>
+      <input id="name-input" />
+      <label>
+        Email
+        <input />
+      </label>
+    </div>
+  );
+}
