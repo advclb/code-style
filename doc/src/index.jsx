b915aa068bc3fadb0244b648c149f9f77591c5fd
Guo Yunhe <i@guoyunhe.me>
2020-01-03 14:46:44 +0200

    add template

--- doc/src/index.jsx
+++ doc/src/index.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-const root = document.createElement("div");
-document.body.append(root);
+const root = document.getElementById("root");
 
 ReactDOM.render(<App />, root);
